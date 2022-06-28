a644bfcf48a36ea6d1d97c98e02a2d51837b33a1
Angus Logan <angus.logan@baronsview.com>
2022-06-29 00:16:15 +0100

    copied the example from bubbletea, added a bit of lipgloss

---
+++ cmd/typing-client/client.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	lg "github.com/charmbracelet/lipgloss"
+	"golang.org/x/term"
+)
+
+type model struct {
+	choices []string // items on the to-do list
+	cursor  int      // which to-do list item our cursor is pointing at
+}
+
+func initialModel() model {
+	return model{
+		// Our shopping list is a grocery list
+		choices: []string{"Race others", "Race yourself"},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "left", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "right", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	physicalWidth, physicalHeight, _ := term.GetSize(int(os.Stdout.Fd()))
+	var container = lg.NewStyle().
+		Width(physicalWidth).
+		Height(physicalHeight).
+		Align(lg.Center)
+
+	// The header
+	var header = lg.NewStyle().
+		Bold(true).
+		Italic(true).
+		Foreground(lg.Color("#588157")).
+		BorderStyle(lg.ThickBorder()).
+		BorderForeground(lg.Color("#344e41")).
+		Padding(1, 3)
+		// MarginTop(container.GetHeight())
+
+	s := header.Render("TYPING.SYSTEMS")
+	s += "\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Render the row
+		s += fmt.Sprintf(" %s [%s] ", cursor, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit."
+
+	// Send the UI for rendering
+	return container.Render(s)
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/racessh/race
 
 go 1.18
+
+require (
+	github.com/charmbracelet/bubbletea v0.22.0
+	github.com/charmbracelet/lipgloss v0.5.0
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
+)
