67a642fdbb821535fe84cdb552b5da23e8b03269
Kamil Zak <kamilzak00@gmail.com>
2022-06-30 19:40:43 +0100

    feat: random word gen

--- cmd/typing-client/utility/utility.go
+++ cmd/typing-client/utility/utility.go
@@ -1,11 +1,54 @@
 package utility
 
 import (
+	"bufio"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
 	lg "github.com/charmbracelet/lipgloss"
 )
 
+func getRandomWord() string {
+	file, err := os.Open("language/words_en")
+	var pick string
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	randSource := rand.NewSource(time.Now().UnixNano())
+	randGenerator := rand.New(randSource)
+
+	lineNum := 1
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		roll := randGenerator.Intn(lineNum)
+		if roll == 0 {
+			pick = line
+		}
+
+		lineNum += 1
+	}
+
+	return pick
+}
+
 func GetRandomSentence(words int) string {
-	return "the quick brown fox jumps over the really lazy dog"
+	arr := make([]string, 5)
+
+	for j := 0; j <= 10; j++ {
+		arr = append(arr, getRandomWord())
+	}
+
+	return strings.Join(arr, " ")
 }
 
 func ForegroundColour(hex string) lg.Style {
